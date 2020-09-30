845a63b2ff027656b8de0dc0c8a0b860767f80b9
Jeans K. Real <jkrb7@hotmail.com>
2020-09-30 15:38:01 -0600

    Finaly, Parcel Quotation doctype and deleted a custom page in app

--- parcel_management/parcel_management/doctype/parcel_quotation/parcel_quotation.js
+++ parcel_management/parcel_management/doctype/parcel_quotation/parcel_quotation.js
@@ -5,6 +5,7 @@ function calculate_product_total_taxes_and_import_price(frm, cdt, cdn) {
     // This func is called on Child Doctype when: product, shipping or taxes are modified.
     let row = locals[cdt][cdn]; // Getting Child Row
 
+    row.shipping_price = row.has_shipping ? row.shipping_price : 0.00;
     row.taxes = row.has_taxes ? flt((row.product_price + row.shipping_price) * 0.07, 2) : 0.00;
 
     row.product_total = flt(row.product_price + row.shipping_price + row.taxes, 2);
@@ -41,11 +42,35 @@ frappe.ui.form.on('Parcel Quotation', {
         );
     },
     refresh: function(frm) {
+        // TODO: Validate the doctype and call save inside here. Se we leverage the validation to frappe
 
         frm.add_custom_button(__('Render Quotation'), () => {
+            const date_string_options = {day: 'numeric', month: 'long', timeZone: 'utc'}; // Settings
+
+            let parsed_items = frm.doc.items.map(item => {
+                let item_to_parse = Object.assign({}, item); // We Copy the item
+
+                item_to_parse.closest_arrival_date = new Date(item.closest_arrival_date).toLocaleDateString('es-ES', date_string_options);
+                item_to_parse.longest_arrival_date = (item.longest_arrival_date) ? new Date(item.longest_arrival_date).toLocaleDateString('es-ES', date_string_options) : null;
+
+                if (item.closest_departure_date) {
+                    item_to_parse.closest_delivery_date = new Date(
+                        moment(item.closest_departure_date).add(frm.doc.transit_days, 'days')
+                    ).toLocaleDateString('es-ES', date_string_options);
+                }
+                if (item.longest_departure_date) {
+                    item_to_parse.longest_delivery_date = new Date(
+                        moment(item.longest_departure_date).add(frm.doc.transit_days, 'days')
+                    ).toLocaleDateString('es-ES', date_string_options);
+                }
+
+                return item_to_parse
+            });
+
+            let doc_to_render = Object.assign({}, frm.doc, {items: parsed_items}); // We change the doc to make date human readable.
 
             $(frm.fields_dict['text'].wrapper)
-                .html(frappe.render_template('parcel_quotation_details', {doc: frm.doc}));
+                .html(frappe.render_template('parcel_quotation_details', {doc: doc_to_render}));
 
             frappe.show_alert({message: 'Success', indicator: 'green'}, 5);
         })
@@ -61,21 +86,12 @@ frappe.ui.form.on('Parcel Quotation Item', {
     shipping_price: function (frm, cdt, cdn) {
         calculate_product_total_taxes_and_import_price(frm, cdt, cdn);
     },
-
     has_shipping: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn]; // Getting Child Row
-
-        if (!row.has_shipping) {
-            row.shipping_price = 0.00; // Reset. and not triggering function
-        }
-
         calculate_product_total_taxes_and_import_price(frm, cdt, cdn);
     },
-
     has_taxes: function (frm, cdt, cdn) {
         calculate_product_total_taxes_and_import_price(frm, cdt, cdn);
     },
-
 
     import_price_type: function (frm, cdt, cdn) {
         calculate_import_price(frm, cdt, cdn);
