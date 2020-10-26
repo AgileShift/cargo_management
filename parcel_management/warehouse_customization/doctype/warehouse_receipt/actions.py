e54012c0b86b34b228fd33ab37a660484fbc27df
Jeans K. Real <jkrb7@hotmail.com>
2020-10-26 12:18:09 -0600

    Warehouse Receipt is now fully able to confirm parcels!

--- parcel_management/warehouse_customization/doctype/warehouse_receipt/actions.py
+++ parcel_management/warehouse_customization/doctype/warehouse_receipt/actions.py
@@ -19,32 +19,29 @@ def confirm_parcels(doc):
     """ Used as Action button in Doctype: Confirms the receipt of the parcels. Change status to: "Awaiting Dispatch" """
     doc = frappe.parse_json(doc)
 
-    wr_lines = doc.get('warehouse_receipt_lines')
+    wr_lines = doc.warehouse_receipt_lines  # Getting all the Parcels
     wr_lines_len = len(wr_lines)
     updated_docs = 0
 
-    frappe.flags.mute_emails, frappe.flags.in_import = True, True
+    # Core: Silence Notifications and emails!
+    frappe.flags.mute_emails, frappe.flags.in_import = doc.mute_emails, doc.mute_emails
 
     for i, wr_line in enumerate(wr_lines, start=1):
-        progress = float(i) * 100 / wr_lines_len
+        progress = i * 100 / wr_lines_len
 
-        parcel = frappe.get_doc('Parcel', wr_line['parcel'])
+        parcel = frappe.get_doc('Parcel', wr_line['parcel'])  # Getting Parcel Doctype
 
-        if parcel.change_status('Awaiting Dispatch'):
-
+        if parcel.change_status('Awaiting Dispatch'):  # If Status can be changed
             updated_docs += 1
-            frappe.flags.mute_emails, frappe.flags.in_import = True, True
-
             parcel.flags.ignore_validate = True  # Set flag ON because Doc will be saved from bulk edit. No validations.
-            parcel.save()
-            # if doc.get('mute_emails'):  # TODO: How to mute emails of status changes?
+            parcel.save(ignore_permissions=True)  # Trigger before_save() who checks for the flag. We avoid all checks.
 
-        print(progress)
+        # TODO: Fix, after publish progress: CTL+S is not working.
         frappe.publish_progress(
-            percent=progress, title='Confirming Parcels', #doctype='Warehouse Receipt', docname=doc.name,
-            description='Confirming Parcel {0}.'.format(parcel.tracking_number)
+            percent=progress, title='Confirming Parcels',
+            description='Confirming Parcel {0}.'.format(parcel.tracking_number),
         )
 
-    print('   ')
+    frappe.flags.mute_emails, frappe.flags.in_import = False, False  # Reset core flags.
 
-    # frappe.msgprint(msg='{0} Parcels confirmed of {1}.'.format(updated_docs, wr_lines_len), title='Success')
+    frappe.msgprint(msg='{0} Parcels confirmed of {1}.'.format(updated_docs, wr_lines_len), title='Success')
