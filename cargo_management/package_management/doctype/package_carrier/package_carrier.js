662ff4bff5d135a20be675b4f06e74afba29b6c1
Jeans K. Real <jkrb7@hotmail.com>
2021-01-07 08:52:55 -0600

    Deleted unused comments

--- cargo_management/package_management/doctype/package_carrier/package_carrier.js
+++ cargo_management/package_management/doctype/package_carrier/package_carrier.js
@@ -1,8 +1,2 @@
-// Copyright (c) 2020, Agile Shift and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Package Carrier', {
-	// refresh: function(frm) {
-
-	// }
 });
