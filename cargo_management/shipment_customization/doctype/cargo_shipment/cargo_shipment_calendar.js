0dd4f1683c88ece7265107149f146f927782122b
Jeans K. Real <jkrb7@hotmail.com>
2021-02-13 06:55:29 -0600

    Cargo Shipment calendar

--- cargo_management/shipment_customization/doctype/cargo_shipment/cargo_shipment_calendar.js
+++ cargo_management/shipment_customization/doctype/cargo_shipment/cargo_shipment_calendar.js
@@ -2,31 +2,30 @@ frappe.views.calendar['Cargo Shipment'] = {
     fields: ['departure_date', 'expected_arrival_date', 'name', 'reference', 'status'],
     field_map: {
         start: 'departure_date',
-        end: 'expected_arrival_date',
+        end: 'expected_arrival_date', // What happens when the cargo shipment is received? end -> must be arrival_date
         id: 'name',
         title: 'reference'
     },
+    gantt: false,
     options: {
         header: {
  			left: 'prev,today,next',
  			center: 'title',
  			right: 'month,agendaWeek,agendaDay'
  		},
-        weekends: true,
+        eventStartEditable: false,  // Cannot edit even if editable is overridden in calendar.js
+        selectable: false,
     },
     get_css_class: function(data) {
-        if (data.status === 'Received')
-            return 'success';
+        if (data.status === 'Received') return 'success';
 
         const days_from_today = frappe.datetime.get_diff(data.expected_arrival_date, frappe.datetime.get_today())
-
         if (days_from_today < 0) {
             return 'danger'; // Is late
         } else if (days_from_today === 0) {
-            return 'default'; // To arrive today!
+            return 'warning'; // To arrive today!
         } else if (days_from_today > 0) {
-            return 'success';
+            return 'default';
         }
-
     }
 }
