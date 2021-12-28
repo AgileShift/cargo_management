84e5ef918d27cea5419fef181c920e875afc59c1
Jeans K. Real <jkrb7@hotmail.com>
2021-12-28 15:18:52 -0600

    Migrating WMS

--- cargo_management/warehouse_customization/doctype/warehouse_receipt/warehouse_receipt.js
+++ cargo_management/warehouse_customization/doctype/warehouse_receipt/warehouse_receipt.js
@@ -1,13 +1,26 @@
 frappe.ui.form.on('Warehouse Receipt', {
 
-    onload: function (frm) {
-	    frm.set_query('package', 'warehouse_receipt_lines', () => {
-            return {
-                filters: {
-                    status: ['not in', ['Awaiting Departure', 'In Transit', 'In Customs', 'Sorting', 'Available to Pickup', 'Finished']]
+    setup: function () {
+        $('.layout-side-section').hide(); // Little Trick to work better
+
+        // https://stackoverflow.com/a/1977126/3172310
+        $(document).on('keydown', "input[data-fieldname='tracking_number'], input[data-fieldname='weight'], " +
+            "input[data-fieldname='length'], input[data-fieldname='width'], input[data-fieldname='height']", (event) => {
+                if (event.key === 'Enter') {  // Enter key is sent if field is set from barcode scanner.
+                    event.preventDefault(); // We prevent the button 'Attach Image' opens a pop-up.
                 }
-            };
-        });
+            });
+    },
+
+    onload: function (frm) {
+        // TODO: WORK THis out
+	    // frm.set_query('package', 'warehouse_receipt_lines', () => {
+        //     return {
+        //         filters: {
+        //             status: ['not in', ['Awaiting Departure', 'In Transit', 'In Customs', 'Sorting', 'Available to Pickup', 'Finished']]
+        //         }
+        //     };
+        // });
     },
 
     refresh: function (frm) {
@@ -18,7 +31,7 @@ frappe.ui.form.on('Warehouse Receipt', {
             return;
         }
 
-        // TODO: Work on this
+        // TODO: Work on this: tjos will be used now?
         if (frm.doc.status === 'Awaiting Departure') {
             frm.page.add_action_item(__('Confirm Packages'), () => {
                 frappe.call({
@@ -37,5 +50,26 @@ frappe.ui.form.on('Warehouse Receipt', {
         // Calculate fields from child so can be saved from client-side
         frm.set_value('warehouse_est_gross_weight', frm.get_sum('warehouse_receipt_lines', 'warehouse_est_weight'));
         frm.set_value('carrier_est_gross_weight', frm.get_sum('warehouse_receipt_lines', 'carrier_est_weight'));
+
+        frm.print_label = frm.is_new(); // If new true else undefined.
+    },
+
+    after_save: function (frm) {
+        if (!frm.print_label) return;
+
+        window.open(
+            frappe.urllib.get_full_url(
+                'printview?doctype=Warehouse%20Receipt&name=' + frm.doc.name +
+                '&trigger_print=1&format=Warehouse%20Receipt%20Labels&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=es'
+            ) // Emulating print.js inside frappe/printing/page/print/print.js
+        );
+    }
+});
+
+// Child Table
+frappe.ui.form.on('Warehouse Receipt Line', {
+    warehouse_est_weight: function (frm) {
+        // TODO: Work this?
+        frm.set_value('warehouse_est_gross_weight', frm.get_sum('warehouse_receipt_lines', 'warehouse_est_weight'));
     }
 });
--- cargo_management/warehouse_customization/doctype/warehouse_receipt/warehouse_receipt_list.js
+++ cargo_management/warehouse_customization/doctype/warehouse_receipt/warehouse_receipt_list.js
@@ -1,9 +1,13 @@
 frappe.listview_settings['Warehouse Receipt'] = {
-    add_fields: ['status'],
+    add_fields: ['status', 'transportation_type'],
     filters: [
         ['status', '!=', 'Finished'],
     ],
     hide_name_column: true,
+
+    before_render() {
+        localStorage.show_sidebar = "false"
+    },
 
     get_indicator(doc) {
         const status_color = {
@@ -14,5 +18,14 @@ frappe.listview_settings['Warehouse Receipt'] = {
         };
 
         return [__(doc.status), status_color[doc.status], 'status,=,' + doc.status];
+    },
+    formatters: {
+        transportation_type(val) {
+            let color = (val === 'Sea') ? 'blue' : 'red';
+            return `<span class="indicator-pill ${color} filterable ellipsis"
+                data-filter="transportation_type,=,${frappe.utils.escape_html(val)}">
+				<span class="ellipsis"> ${val} </span>
+			<span>`;
+        }
     }
 }
