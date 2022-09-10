418a37b81922853378b5730c35b026d6ba28354a
Jeans K. Real <jkrb7@hotmail.com>
2022-09-10 12:56:50 -0600

    Working on Auto Update of Parcels when the easypost cannot track

--- cargo_management/hooks.py
+++ cargo_management/hooks.py
@@ -15,8 +15,8 @@ source_link = "https://github.com/AgileShift/cargo_management"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = ["cargo_management.bundle.css"]
-app_include_js = ["cargo_management.bundle.js"]
+app_include_css = "cargo_management.bundle.css"
+app_include_js = "cargo_management.bundle.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/cargo_management/css/package_management.css"
@@ -79,10 +79,7 @@ app_include_js = ["cargo_management.bundle.js"]
 # 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
-# Document Events
-# ---------------
-# Hook on document methods and events
-
+# Document Events: Hook on document methods and events
 doc_events = {
 	"Sales Invoice": {
 		"on_submit": "cargo_management.sales_customization.utils.sales_invoice_on_submit"
@@ -90,25 +87,11 @@ doc_events = {
 }
 
 # Scheduled Tasks
-# ---------------
-
-# scheduler_events = {
-# 	"all": [
-# 		"cargo_management.tasks.all"
-# 	],
-# 	"daily": [
-# 		"cargo_management.tasks.daily"
-# 	],
-# 	"hourly": [
-# 		"cargo_management.tasks.hourly"
-# 	],
-# 	"weekly": [
-# 		"cargo_management.tasks.weekly"
-# 	]
-# 	"monthly": [
-# 		"cargo_management.tasks.monthly"
-# 	]
-# }
+scheduler_events = {
+	"all": [
+		"cargo_management.package_management.doctype.parcel.events.check_parcel_delivery"
+	],
+}
 
 # Testing
 # -------
---
+++ cargo_management/package_management/doctype/parcel/events.py
@@ -0,0 +1,13 @@
+import frappe
+
+
+def check_parcel_delivery():
+    pass
+
+    #parcel = frappe.get_doc('Parcel', 'TESTING')
+
+    #parcel.consolidated_tracking_numbers = frappe.utils.random_string(20)
+
+    #parcel.flags.ignore_permissions = True
+    #parcel.flags.ignore_mandatory = True
+    #parcel.save()
