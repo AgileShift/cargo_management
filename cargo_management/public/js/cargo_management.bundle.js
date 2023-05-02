e11d16ed9aa89654e95c72085b6060d1c46b4e5b
Jeans K. Real <jkrb7@hotmail.com>
2023-05-01 20:03:47 -0600

    Refactored Function that finds a carryer by the trackig number

--- cargo_management/public/js/cargo_management.bundle.js
+++ cargo_management/public/js/cargo_management.bundle.js
@@ -6,78 +6,43 @@ frappe.provide('cargo_management');
 
 cargo_management = {
 	find_carrier_by_tracking_number: function (tracking_number) {
-        /* LINKED WITH: package_management/doctype/parcel/actions.py -> find_carrier_by_tracking_number
+        /* This JavaScript function replicates the corresponding Python function to prevent unnecessary API calls.
+        By duplicating the functionality in JS, we can avoid making extra calls to the Python API.
+        LINKED WITH: package_management/doctype/parcel/actions.py -> find_carrier_by_tracking_number */
 
-        Its specially made to avoid a call the server(API) to get a response that's does not have database data
-
-        - https://pages.message.fedex.com/barcodescan_home/
-
-        // TODO: Add More Carriers
-        // FIXME: if only TBA, 1Z, 1LS is sent it should fail
-        // FIXME: Run if tracking_number length is more than 6?
-        */
         tracking_number = tracking_number.trim().toUpperCase();  // Sanitize field
 
-        if (!tracking_number)
-            return {};
+		if (!tracking_number || tracking_number.length <= 4)
+			return {};
 
-        // TODO: search_term = 2/4 and 3/4 of the tracking. At bottom?
-        let carrier = 'Unknown', search_term = tracking_number, tracking_number_len = tracking_number.length;
+		const carrierRegex = [
+			{carrier: 'UPS',        regex: /^1Z/},
+			{carrier: 'SunYou',     regex: /^SY/},       // SYUS & SYAE & SYBA
+			{carrier: 'SF Express', regex: /^SF/},
+			{carrier: 'Amazon',     regex: /^TBA/},
+			{carrier: 'Cainiao',    regex: /^LP00/},     // Sometimes Cainiao can track 'Yanwen' and 'SunYou'
+			{carrier: 'DHL',        regex: /^.{10}$/},
+			{carrier: 'YunExpress', regex: /^YT|^YU00/}, // Sometimes these are delivered by USPS and LaserShip
+			{carrier: 'LaserShip',  regex: /^1LS|^D100/},
+			{carrier: 'Yanwen',     regex: /^ALS00|^S000|^UY/}, // ALS00 sometimes delivered by USPS. UY ends with 'CZ'
+			{carrier: 'Unknown',    regex: /^92(612.{17})$|^420.{5}92(612.{17})$/},       // *92612*90980949456651012 | 42033166*926129*0980949456651012. Start with: 92612 or with zipcode(420xxxxx) can be handled by FedEx or USPS. search_term starts at 612
+			{carrier: 'USPS',       regex: /^9(?:.{21}|.{25})$|^420.{5}(9(?:.{21}|.{25}))$/}, // *9*400111108296364807659 | 42033165*9*274890983426386918697. First 8 digits: 420xxxxx(zipcode)
+			{carrier: 'FedEx',      regex: /^.{12}$|^612.{17}$|^.{22}([1-9].{11})$/},    // *612*90982157320543198 | 9622001900005105596800*5*49425980480. Last 12 digits is tracking
+		]; // FIXME: Sort by the most used Carrier | Add More Carriers: 'LY', 'LB', 'LW'
 
-        if (tracking_number.slice(0, 3) === 'TBA') {
-            carrier = 'Amazon';
-        } else if (tracking_number.slice(0, 2) === '1Z') {
-            carrier = 'UPS';
-        } else if (tracking_number.slice(0, 3) === '1LS' || tracking_number.slice(0, 2) === 'D10') {
-            carrier = 'LaserShip';
-        } else if (tracking_number.slice(0, 4) === 'LP00') {
-            carrier = 'Cainiao'; // Sometimes Cainiao can track 'Yanwen' and 'SunYou'
-        } else if (tracking_number.slice(0, 2) === 'SF') {
-            carrier = 'SF Express';
-        } else if (tracking_number.slice(0 ,5) === 'ALS00' || tracking_number.slice(0, 3) === 'S00' || tracking_number.slice(0, 2) === 'UY') {
-            carrier = 'Yanwen';
-        } else if (tracking_number.slice(0, 2) === 'YT') {
-            carrier = 'YunExpress';
-        } else if (tracking_number.slice(0, 2) === 'SY') { //  SYUS && SYAE
-            carrier = 'SunYou';
-        }
+		const result = carrierRegex.find(carrier => {
+			const match = tracking_number.match(carrier.regex);
 
-        else if (tracking_number_len === 10) {
-            carrier = 'DHL';
-        } else if (tracking_number.slice(0, 2) === 'JD') {  // or JJD ?
-            carrier = 'DHL';  // FIXME: Maybe we can convert it?
-            frappe.show_alert({indicator: 'orange', message: __('Convert to DHL Tracking')});
-        }
+			if (match) {  // We add the capture group if any and exit the loop.
+				carrier.search_term = match[1] || match[2];
+				return true;
+			}
+		});
 
-        // TODO: else if ( any(s in tracking_number for s in ['LY', 'LB', 'LW']) )
-        //     return 'Possibly a USPS Tracking' || Can be tracked with Cainiao and USPS
-        // TODO: YT: eg: YT2219421236038447
-
-        // FedEx or USPS. Matches starting with: 92612 or with zipcode(420xxxxx). To search we will return starting with 612
-        else if (tracking_number_len === 22 && tracking_number.slice(0, 5) === '92612') {
-            carrier = ''; search_term = tracking_number.slice(2);        // *92612*90980949456651012. Or *926129*
-        } else if (tracking_number_len === 30 && tracking_number.slice(8, 13) === '92612') {
-            carrier = ''; search_term = tracking_number.slice(10);       // *42033166*92612*90980949456651012. Or *926129*
-        }
-
-        else if ([22, 26].includes(tracking_number_len) && tracking_number[0] === '9') {
-            carrier = 'USPS';                                            // *9*400111108296364807659
-        } else if ([30, 34].includes(tracking_number_len) && tracking_number[8] === '9') {
-            carrier = 'USPS'; search_term = tracking_number.slice(8);    // 42033165*9*274890983426386918697. First 8 digits: 420xxxxx(zipcode)
-        }
-
-        else if (tracking_number_len === 12) {
-            carrier = 'FedEx';
-        } else if (tracking_number_len === 20 && tracking_number.slice(0, 3) === '612') {
-            carrier = 'FedEx';                                           // *612*90982157320543198. Or *6129*
-        } else if (tracking_number_len === 34 && tracking_number[22] !== '0') {
-            carrier = 'FedEx'; search_term = tracking_number.slice(22);  // 9622001900005105596800*5*49425980480. Last 12 digits is tracking
-        }
-
-        return {
-            'carrier': carrier,
-            'search_term': search_term,
-            'tracking_number': tracking_number
+		return {
+			'tracking_number': tracking_number,
+            'carrier': result?.carrier ?? 'Unknown',
+            'search_term': result?.search_term ?? tracking_number // If no captured group, then return whole string.
         };
     },
     transportation_icon_html: function (transportation) {
@@ -99,3 +64,4 @@ cargo_management = {
         return {api, urls};
     }
 };
+//102 - Optimizing code -> 96
