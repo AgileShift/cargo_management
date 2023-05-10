865bfeb25c33860d197d5ca50d68830c3bd70b42
Jeans K. Real <jkrb7@hotmail.com>
2023-05-10 16:05:39 -0600

    Fixing a problem when the tracking is too short wont return anything and fields was being erased

--- cargo_management/public/js/cargo_management.bundle.js
+++ cargo_management/public/js/cargo_management.bundle.js
@@ -6,41 +6,39 @@ frappe.provide('cargo_management');
 
 cargo_management = {
 	find_carrier_by_tracking_number: function (tracking_number) {
-        tracking_number = tracking_number.trim().toUpperCase();  // Sanitize field
+		tracking_number = tracking_number.trim().toUpperCase(); // Sanitize field
 
-		if (!tracking_number || tracking_number.length <= 3)
-			return {}; // FIXME: We should return something? or avoid this validation.
+		let response = {carrier: 'Unknown', search_term: tracking_number, tracking_number};
 
-		const carrierRegex = [ // USPS and FedEx the order matters!
+		if (!tracking_number || tracking_number.length <= 6)
+			return response; // If data is not returned, fields will be erased. Affected Views: List, Form and QuickEntry
+
+		const carrierRegex = [ // The order matters for USPS and FedEx!
 			{carrier: 'UPS',        regex: /^1Z/},
 			{carrier: 'SunYou',     regex: /^SY/},       // SYUS & SYAE & SYBA
 			{carrier: 'SF Express', regex: /^SF/},
 			{carrier: 'Amazon',     regex: /^TBA/},
-			{carrier: 'Cainiao',    regex: /^LP00/},     // Sometimes Cainiao can track 'Yanwen' and 'SunYou'
+			{carrier: 'Cainiao',    regex: /^LP00/},     // Cainiao can sometimes track 'Yanwen' and 'SunYou'
 			{carrier: 'DHL',        regex: /^.{10}$/},
-			{carrier: 'YunExpress', regex: /^YT|^YU00/}, // Sometimes these are delivered by USPS and LaserShip
+			{carrier: 'YunExpress', regex: /^YT|^YU00/}, // These are sometimes delivered by USPS and LaserShip
 			{carrier: 'LaserShip',  regex: /^1LS|^D100/},
-			{carrier: 'Yanwen',     regex: /^ALS00|^S000|^UY/}, // ALS00 sometimes delivered by USPS. UY ends with 'CZ'
+			{carrier: 'Yanwen',     regex: /^ALS00|^S000|^UY/}, // ALS00 is sometimes delivered by USPS. UY ends with 'CZ'
 			{carrier: 'Unknown',    regex: /^92(612.{17})$|^420.{5}92(612.{17})$/},       // *92612*90980949456651012 | 42033166*926129*0980949456651012. Start with: 92612 or with zipcode(420xxxxx) can be handled by FedEx or USPS. search_term starts at 612
 			{carrier: 'USPS',       regex: /^9(?:.{21}|.{25})$|^420.{5}(9(?:.{21}|.{25}))$/}, // *9*400111108296364807659 | 42033165*9*274890983426386918697. First 8 digits: 420xxxxx(zipcode)
-			{carrier: 'FedEx',      regex: /^.{12}$|^612.{17}$|^.{22}([1-9].{11})$/},    // *612*90982157320543198 | 9622001900005105596800*5*49425980480. Last 12 digits is tracking
-		]; // FIXME: Sort by the most used Carrier | FIXME: Add More Carriers: 'LY', 'LB', 'LW'
+			{carrier: 'FedEx',      regex: /^.{12}$|^612.{17}$|^.{22}([1-9].{11})$/},     // *612*90982157320543198 | 9622001900005105596800*5*49425980480. Last 12 digits is tracking
+		]; // FIXME: Sort by the most used Carrier? | FIXME: Add More Carriers: 'LY', 'LB', 'LW' | # FIXME: Move to carriers.json
 
-		const result = carrierRegex.find(carrier => {
-			const match = tracking_number.match(carrier.regex);
+		carrierRegex.find(({carrier, regex}) => {
+			const match = tracking_number.match(regex);
 
 			if (match) {
-				carrier.search_term = match[1] || match[2];  // We add the capture group if any and exit the loop.
+				Object.assign(response, {carrier, search_term: match[1] || match[2] || tracking_number}); // If a captured group exists add it
 				return true;
 			}
 		});
 
-		return {
-			'tracking_number': tracking_number,
-            'carrier': result?.carrier ?? 'Unknown',
-            'search_term': result?.search_term ?? tracking_number // If no captured group, then return whole string.
-        };
-    },
+		return response; // If no match is found, default values will be returned.
+	},
     transportation_icon_html: function (transportation) {
         return ` <i class="fa fa-${transportation === 'Sea' ? 'ship' : 'plane'}"></i>`; // Watch the first whitespace.
     },
