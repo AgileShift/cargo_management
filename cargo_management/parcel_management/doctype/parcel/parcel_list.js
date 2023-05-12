add3252e53eb6092b91716b77f5b9f855fc89b08
Jeans K. Real <jkrb7@hotmail.com>
2023-05-11 22:33:10 -0600

    Better Comments, and some typo fix. Nothing to see.

--- cargo_management/parcel_management/doctype/parcel/parcel_list.js
+++ cargo_management/parcel_management/doctype/parcel/parcel_list.js
@@ -4,10 +4,10 @@ frappe.listview_settings['Parcel'] = {
     hide_name_column: false,
 
     onload: function (listview) {
-        const {name: name_field, tracking_number: tracking_number_fields, customer_name: customer_name_field} = listview.page.fields_dict;
+        const {name: name_field, tracking_number: tracking_number_field, customer_name: customer_name_field} = listview.page.fields_dict;
 
         // Remove tracking_number field from Standard Filter (it's set because the field is the title of the Doctype)
-        tracking_number_fields.$wrapper.remove();
+        tracking_number_field.$wrapper.remove();
 
         // Update placeholder and help-text for Name field
         name_field.$wrapper.attr('data-original-title', __('Tracking Number'));
@@ -16,38 +16,33 @@ frappe.listview_settings['Parcel'] = {
         // Override: onchange() method set in make_standard_filters(). We call refresh_list_view() if value has changed.
         name_field.df.onchange = customer_name_field.df.onchange = function () {
             this.value = this.input.value = this.get_input_value().trim().toUpperCase();  // Change internal and UI value
-            //if (this.value !== this.last_value) {
+            if (this.value !== this.last_value) {
                 listview.filter_area.refresh_list_view(); // Same as make_standard_filters()
-
-			console.log('on change event!');
-            //}
+            }
         }
 
         // TODO: listview.get_count_str() => This call frappe.db.count() using 'filters' not 'or_filters'
         // TODO: listview.list_sidebar.get_stats() => This call frappe.desk.reportview.get_sidebar_stats using 'filters' not 'or_filters'
-        // Override: Use the 'search_term' of the 'name' field in the 'or_filters', remove '%' added in get_standard_filters()
+
+		// We Override to add: or_filters and Remove '%' added in get_standard_filters
         listview.get_args = () => {
-            // Removing '%' added when the listview loads first time. Also sanitize field and change UI
-            name_field.value = name_field.input.value = name_field.get_input_value().replaceAll('%', '');
+
+			// Removing '%' added when the listview loads first time. Also sanitize field and change UI
+			name_field.value = name_field.input.value = name_field.get_input_value().replaceAll('%', '');
             customer_name_field.value = customer_name_field.input.value = customer_name_field.get_input_value().replaceAll('%', '');
 
             let args = frappe.views.ListView.prototype.get_args.call(listview);  // Calling his super for the args
 
             const name_filter = args.filters.findIndex(f => f[1] === 'name');  // f -> ['Doctype', 'field', 'sql_search_term', 'value']
 
-            if (name_filter >= 0) {  // We have 'name' filter being filtered. -> Will return index if found
+            if (name_filter >= 0) {  // We have 'name' filter being filtered. -> name_filter will contain index if found
                 args.filters.splice(name_filter, 1);  // Removing 'name' filter from 'filters'. It's a 'standard_filter'
 
-                const data = cargo_management.find_carrier_by_tracking_number(name_field.get_input_value());
+				const search_term = cargo_management.find_carrier_by_tracking_number(name_field.get_input_value()).search_term;
 
-				console.log('Name filter is being touched!????');
-				console.log(data);
-
-                args.or_filters = [
-                    [args.doctype, 'name', 'like', '%' + data.search_term + '%'],
-                    [args.doctype, 'tracking_number', 'like', '%' + data.search_term + '%'],
-                    [args.doctype, 'consolidated_tracking_numbers', 'like', '%' + data.search_term + '%']
-                ];
+				args.or_filters = ['name', 'tracking_number', 'consolidated_tracking_numbers'].map(field => [
+					args.doctype, field, 'like', '%' + search_term + '%'
+				]); // Mapping each field to 'or_filters' for the necessary fields to search
             }
 
             return args;
@@ -55,8 +50,8 @@ frappe.listview_settings['Parcel'] = {
     },
 
     get_indicator: function (doc) {
-        // TODO: Migrate to Document States?
-        //Unused: Light Blue
+        // TODO: Migrate to Document States? Maybe when frappe core starts using it.
+        // Unused: Light Blue
         return [__(doc.status), {
             'Awaiting Receipt': 'blue',
             'Awaiting Confirmation': 'orange',
@@ -76,17 +71,17 @@ frappe.listview_settings['Parcel'] = {
     },
 
     button: {
-        show(doc) {
-            return doc.name;
+        show() {
+            return true;
         },
         get_label() {
-            return __('Carrier page')
+            return __('Carrier page');
         },
         get_description() {
-            return __('Open carrier page')
+            return __('Open carrier page');
         },
         action(doc) {
-            // TODO: Make this a List of buttons
+            // TODO: Make this a Dropdown. We need to override: setup_action_handler and get_meta_html of list_view.js in the onload method in this file
             window.open(cargo_management.load_carrier_settings(doc.carrier).urls[0].url + doc.tracking_number, '_blank');
         },
     },
