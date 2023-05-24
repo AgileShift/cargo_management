311664cc1368e958a8924ff792dff4405fd2f24a
Jeans K. Real <jkrb7@hotmail.com>
2023-05-23 18:52:08 -0600

    Parcel List Custom Button as DropDown

--- cargo_management/parcel_management/doctype/parcel/parcel_list.js
+++ cargo_management/parcel_management/doctype/parcel/parcel_list.js
@@ -1,31 +1,30 @@
 frappe.listview_settings['Parcel'] = {
-    add_fields: ['carrier'],
-    filters: [['status', 'not in', ['Finished', 'Cancelled', 'Never Arrived', 'Returned to Sender']]],
-    hide_name_column: false,
+	add_fields: ['carrier'],
+	filters: [['status', 'not in', ['Finished', 'Cancelled', 'Never Arrived', 'Returned to Sender']]],
 
-    onload: function (listview) {
-        const {name: name_field, tracking_number: tracking_number_field, customer_name: customer_name_field} = listview.page.fields_dict;
+	onload(listview) {
+		const {name: name_field, tracking_number: tracking_number_field, customer_name: customer_name_field} = listview.page.fields_dict;
 
-        // Remove tracking_number field from Standard Filter (it's set because the field is the title of the Doctype)
-        tracking_number_field.$wrapper.remove();
+		// Remove tracking_number field from Standard Filter (it's set because the field is the title of the Doctype)
+		tracking_number_field.$wrapper.remove();
 
-        // Update placeholder and help-text for Name field
-        name_field.$wrapper.attr('data-original-title', __('Tracking Number'));
-        name_field.$input.attr('placeholder', __('Tracking Number'));
+		// Update placeholder and help-text for Name field
+		name_field.$wrapper.attr('data-original-title', __('Tracking Number'));
+		name_field.$input.attr('placeholder', __('Tracking Number'));
 
-        // Override: onchange() method set in make_standard_filters(). We call refresh_list_view() if value has changed.
-        name_field.df.onchange = customer_name_field.df.onchange = function () {
-            this.value = this.input.value = this.get_input_value().trim().toUpperCase();  // Change internal and UI value
-            if (this.value !== this.last_value) {
-                listview.filter_area.refresh_list_view(); // Same as make_standard_filters()
-            }
-        }
+		// Override: onchange() method set in make_standard_filters(). We call refresh_list_view() if value has changed.
+		name_field.df.onchange = customer_name_field.df.onchange = function () {
+			this.value = this.input.value = this.get_input_value().trim().toUpperCase();  // Change internal and UI value
+			if (this.value !== this.last_value) {
+				listview.filter_area.refresh_list_view(); // Same as make_standard_filters()
+			}
+		}
 
-        // TODO: listview.get_count_str() => This call frappe.db.count() using 'filters' not 'or_filters'
-        // TODO: listview.list_sidebar.get_stats() => This call frappe.desk.reportview.get_sidebar_stats using 'filters' not 'or_filters'
+		// TODO: listview.get_count_str() => This call frappe.db.count() using 'filters' not 'or_filters'
+		// TODO: listview.list_sidebar.get_stats() => This call frappe.desk.reportview.get_sidebar_stats using 'filters' not 'or_filters'
 
 		// We Override to add: or_filters and Remove '%' added in get_standard_filters
-        listview.get_args = () => {
+		listview.get_args = function () {
 
 			// Removing '%' added when the listview loads first time. Also sanitize field and change UI
 			name_field.value = name_field.input.value = name_field.get_input_value().replaceAll('%', '');
@@ -47,47 +46,48 @@ frappe.listview_settings['Parcel'] = {
 
             return args;
         }
+
+		// Override to add a dropdown. If the 'show' function within the 'button' property is null or returns False it won't create the 'settings_button'
+		listview.get_meta_html = function (doc) {
+			let $html = $(frappe.views.ListView.prototype.get_meta_html.call(listview, doc)); // Calling his super for the html string value
+
+			const urls = cargo_management.load_carrier_settings(doc.carrier).urls.reduce((acc, url) => {
+				return acc + `<a class="dropdown-item" href="${url.url + doc.tracking_number}" target="_blank">${url.title}</a>`;
+			}, ''); // Loading the Carrier URLs then return a single string of items for the dropdown
+
+			// Find the parent element of 'settings_button' and 'assigned_to', then replace with our custom 'button'. Check the core function on list_view.js
+			$html.find('.hidden-md.hidden-xs').append(`
+				<div class="list-actions dropdown">
+					<a class="btn btn-default btn-xs dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${__('Carriers')}</a>
+					<div class="dropdown-menu dropdown-menu-right">${urls}</div>
+				</div>
+			`);
+			console.log($html.prop('outerHTML'));
+
+			return $html.prop('outerHTML');
+		}
     },
 
-    get_indicator: function (doc) {
-        // TODO: Migrate to Document States? Maybe when frappe core starts using it.
-        // Unused: Light Blue
-        return [__(doc.status), {
-            'Awaiting Receipt': 'blue',
-            'Awaiting Confirmation': 'orange',
-            'In Extraordinary Confirmation': 'pink',
-            'Awaiting Departure': 'yellow',
-            'In Transit': 'purple',
-            'In Customs': 'gray',
-            'Sorting': 'green',
-            'To Bill': 'green',
-            'Unpaid': 'red',
-            'To Deliver or Pickup': 'cyan',
-            'Finished': 'darkgrey',
-            'Cancelled': 'red',
-            'Never Arrived': 'red',
-            'Returned to Sender': 'red',
-        }[doc.status], 'status,=,' + doc.status];
-    },
+	// Unused: Light Blue. // TODO: Migrate to Document States? Maybe when frappe core starts using it.
+	get_indicator: (doc) => [__(doc.status), {
+		'Awaiting Receipt': 'blue',
+		'Awaiting Confirmation': 'orange',
+		'In Extraordinary Confirmation': 'pink',
+		'Awaiting Departure': 'yellow',
+		'In Transit': 'purple',
+		'In Customs': 'gray',
+		'Sorting': 'green',
+		'To Bill': 'green',
+		'Unpaid': 'red',
+		'To Deliver or Pickup': 'cyan',
+		'Finished': 'darkgrey',
+		'Cancelled': 'red',
+		'Never Arrived': 'red',
+		'Returned to Sender': 'red',
+	}[doc.status], 'status,=,' + doc.status],
 
-    button: {
-        show() {
-            return true;
-        },
-        get_label() {
-            return __('Carrier page');
-        },
-        get_description() {
-            return __('Open carrier page');
-        },
-        action(doc) {
-            // TODO: Make this a Dropdown. We need to override: setup_action_handler and get_meta_html of list_view.js in the onload method in this file
-            window.open(cargo_management.load_carrier_settings(doc.carrier).urls[0].url + doc.tracking_number, '_blank');
-        },
-    },
-
-    formatters: {
-        transportation: (value) => cargo_management.transportation_formatter(value),
-        name: (value, df, doc) => (value !== doc.tracking_number) ? value.bold() : ''
-    }
+	formatters: {
+		transportation: (value) => cargo_management.transportation_formatter(value),
+		name: (value, df, doc) => (value !== doc.tracking_number) ? value.bold() : ''
+	}
 };
