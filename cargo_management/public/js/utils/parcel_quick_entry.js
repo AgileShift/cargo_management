7e649ee9dfbf5f7e6e5d37e8a1c03e1cdb4a4857
Jeans K. Real <jkrb7@hotmail.com>
2023-05-30 22:06:34 -0600

    Added Extra Fields and a Custom MultiCheckUnique for Transportation Field

--- cargo_management/public/js/utils/parcel_quick_entry.js
+++ cargo_management/public/js/utils/parcel_quick_entry.js
@@ -5,22 +5,14 @@ frappe.ui.form.ParcelQuickEntryForm = class ParcelQuickEntryForm extends frappe.
 		super(doctype, after_insert, init_callback, doc, force);
 	}
 
-	set_meta_and_mandatory_fields() {
-		super.set_meta_and_mandatory_fields();
-
-		// FIXME: Changes fields before calling render_dialog(); This approach can fail miserably and is not recommended
-		[this.mandatory[1], this.mandatory[2], this.mandatory[3]] = [this.mandatory[2],  this.mandatory[3], this.mandatory[1]]; // Swap Order
-		this.mandatory.splice(3, 0, {fieldtype: "Column Break"}); // Insert new field on custom index
-	}
-
 	render_dialog() {
-		this.mandatory.push(...this.get_extra_fields());  // Adding a Table Field after executing is_quick_entry()
+		this.add_extra_fields(); // Adding Table Field after executing is_quick_entry()
 		super.render_dialog();
-		this.init_post_render_dialog_operations(); // Custom function to modify DOM properties after the dialog has been rendered
+		this.init_post_render_dialog_operations(); // Custom method to modify DOM properties after the dialog has been rendered
 	}
 
 	init_post_render_dialog_operations() {
-		let {carrier, tracking_number, extra_services_section, content_section, content} = this.dialog.fields_dict;
+		let {carrier, tracking_number, extra_services_section, content_section, content, transportation_options} = this.dialog.fields_dict;
 
 		tracking_number.df.onchange = function () {  // Override onchange to sanitize field and set carrier
 			const data = cargo_management.find_carrier_by_tracking_number(this.get_input_value());
@@ -30,21 +22,32 @@ frappe.ui.form.ParcelQuickEntryForm = class ParcelQuickEntryForm extends frappe.
 		};
 
 		this.dialog.$wrapper.find('.modal-dialog').addClass('modal-lg'); // Making the Dialog large
-		extra_services_section.wrapper.insertAfter(carrier.parent).removeClass('row visible-section'); // Moving an entire section within a column
+		extra_services_section.wrapper.insertAfter(carrier.wrapper).removeClass('row visible-section'); // Moving an entire section within a column
 
 		// Styling the content section
+		transportation_options.$wrapper.addClass('text-center');
 		content_section.wrapper.css({'padding-top': 0, 'border': 'none'});
 		content_section.body.css('margin-top', 0);
 		content.$wrapper.find('.control-label').hide(); // Hiding Label of Content Table
 	}
 
-	get_extra_fields() {
-		// TODO: // Item code with filters. Maybe we can filter if its is by the service Type?
-		return [
+	add_extra_fields() {
+		// FIXME: This approach can fail miserably and is not recommended
+		[this.mandatory[1], this.mandatory[2]] = [this.mandatory[2], this.mandatory[1]]; // Swap Order: Customer with Carrier
+
+		// Insert New Fields in between Customer and Carrier
+		this.mandatory.splice(2, 0,{
+			fieldtype: 'MultiCheckSingle', fieldname: 'transportation_options', label: __('Transportation'), reqd: 1, columns: 2,
+			options: [{label: __('SEA'), value: 'Sea'}, {label: __('AIR'), value: 'Air'}],
+			on_change: (selected) => this.doc.transportation = selected // Bind the selected checkbox with the doc field
+		}, {fieldtype: "Column Break"});
+
+		this.mandatory.push(
 			{fieldtype: 'Section Break', fieldname: 'content_section'},
 			{fieldtype: 'Table', fieldname: 'content', options: 'Parcel Content', in_place_edit: false, fields: [
-				{label: __('Description'), fieldtype: 'Data', fieldname: 'description', in_list_view: true},
-				{label: __('Item Code'), fieldtype: 'Link', fieldname: 'item_code', options: 'Item', in_list_view: true}]}
-		];
+				{label: __('Description'), fieldtype: 'Small Text', fieldname: 'description', in_list_view: true},
+				{label: __('Item Code'), fieldtype: 'Link', fieldname: 'item_code', options: 'Item', in_list_view: true}
+			]}
+		); // TODO: Item code with filters. Maybe we can filter if its is by the service Type?
 	}
-}
+};
