c29d3eba4d0a3a98220610ab3f1120079b2c7caf
Jeans K. Real <jkrb7@hotmail.com>
2023-06-02 12:28:10 -0600

    Remove Override of ControlMultiCheck fixed on https://github.com/frappe/frappe/pull/21204. MulticheckSingle can set_bold now

--- cargo_management/public/js/controls/multichecksingle.js
+++ cargo_management/public/js/controls/multichecksingle.js
@@ -1,22 +1,23 @@
-// Override ControlMultiCheck to create a MultiCheckSingle -> See: frappe/form/controls/text.js
-frappe.ui.form.ControlMultiCheckSingle = class ControlMultiCheckUnique extends frappe.ui.form.ControlMultiCheck {
+frappe.ui.form.ControlMultiCheckSingle = class ControlMultiCheckSingle extends frappe.ui.form.ControlMultiCheck {
+	// Override ControlMultiCheck to create a MultiCheckSingle -> See: frappe/form/controls/text.js
 
 	refresh_input() {
 		// Override this method, We drew inspiration from: /form/controls/base_input.js -> refresh_input();
 		this.set_mandatory();
 		this.set_required();
+		this.set_bold();
 	}
 
-    bind_checkboxes() {
+	bind_checkboxes() {
 		// Override to enforce a single checkbox selection and return the selected option on change
 		// Refer to: /form/controls/multicheck.js -> bind_checkboxes();
-        $(this.wrapper).on('change', ':checkbox', (e) => {
-            const $checkbox = $(e.target);
+		$(this.wrapper).on('change', ':checkbox', (e) => {
+			const $checkbox = $(e.target);
 
-            if ($checkbox.is(':checked')) {
-                this.$checkbox_area.find('input').not($checkbox).prop('checked', false); // Uncheck Others
-                this.selected_options = $checkbox.attr("data-unit");
-            } else {
+			if ($checkbox.is(':checked')) {
+				this.$checkbox_area.find('input').not($checkbox).prop('checked', false); // Uncheck Others
+				this.selected_options = $checkbox.attr("data-unit");
+			} else {
 				this.selected_options = ''; // If unchecked, set to empty
 			}
 
@@ -25,18 +26,22 @@ frappe.ui.form.ControlMultiCheckSingle = class ControlMultiCheckUnique extends f
 			}
 
 			// Call the on_change function with the selected option. This is useful for binding to other fields.
-            this.df.on_change && this.df.on_change(this.selected_options);
-        });
-    }
+			this.df.on_change && this.df.on_change(this.selected_options);
+		});
+	}
 
+	// Refer to: /form/controls/base_input.js -> set_mandatory() & set_invalid() & set_required() & set_bold()
 	set_mandatory() {
 		// Calling a core method to avoid DRY. This is necessary because MultiCheck inherits from Control rather than ControlInput(parent)
-		frappe.ui.form.ControlInput.prototype.set_mandatory.call(this, this.selected_options); // Refer to: /form/controls/base_input.js -> set_mandatory();
+		frappe.ui.form.ControlInput.prototype.set_mandatory.call(this, this.selected_options);
 	}
 	set_invalid() {
-		this.$wrapper.toggleClass("has-error", !this.selected_options); // Refer to: /form/controls/base_input.js -> set_invalid();
+		this.$wrapper.toggleClass("has-error", !this.selected_options);
 	}
 	set_required() {
-		this.$label.toggleClass('reqd', Boolean(this.df.reqd)); // Refer to: /form/controls/base_input.js -> set_required();
+		this.$label.toggleClass('reqd', this.df.reqd);
+	}
+	set_bold() {
+		this.$checkbox_area.toggleClass('bold', this.df.bold);
 	}
 };
--- cargo_management/public/js/controls/overrides.js
+++ cargo_management/public/js/controls/overrides.js
@@ -1,17 +1,7 @@
 frappe.ui.form.ControlSmallText = class ControlSmallText extends frappe.ui.form.ControlSmallText {
-    // Refer to: frappe/form/controls/text.js
-    make_input() {
-        super.make_input();
-        this.$input.css('height', '');  // Remove the inline 'height' CSS property from the input element
-    }
-};
-
-frappe.ui.form.ControlMultiCheck = class ControlMultiCheck extends frappe.ui.form.ControlMultiCheck {
-	// Override the refresh() method in ControlMultiCheck to avoid extra calls to refresh_input()
-	// For more details, refer to: /form/controls/multicheck.js and its parent: /form/controls/base_control.js
-	refresh() {
-		this.set_options();     // As Parent
-		this.bind_checkboxes(); // As Parent
-		frappe.ui.form.Control.prototype.refresh.call(this); // bypasses the extra calls to refresh_input(). It's equivalent to calling super.super.
+	// Refer to: frappe/form/controls/text.js
+	make_input() {
+		super.make_input();
+		this.$input.css('height', '');  // Remove the inline 'height' CSS property from the input element
 	}
 };
