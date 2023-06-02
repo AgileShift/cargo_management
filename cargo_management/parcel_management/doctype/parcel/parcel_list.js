d1e6960499608a6aa7c97448182b130abd003d50
Jeans K. Real <jkrb7@hotmail.com>
2023-06-02 11:40:40 -0600

    Removed deprecated bold() and also migrate code from jquery to builtin methods

--- cargo_management/parcel_management/doctype/parcel/parcel_list.js
+++ cargo_management/parcel_management/doctype/parcel/parcel_list.js
@@ -3,32 +3,33 @@ frappe.listview_settings['Parcel'] = {
 	filters: [['status', 'not in', ['Finished', 'Cancelled', 'Never Arrived', 'Returned to Sender']]],
 
 	onload(listview) {
+		//frappe.ui.form.make_quick_entry('Parcel') // TODO: DELETE THIS. It's just for testing
+
 		const {name: name_field, tracking_number: tracking_number_field, customer_name: customer_name_field} = listview.page.fields_dict;
 
-		// Remove tracking_number field from Standard Filters UI. It's set because the field is the title of the Doctype
-		tracking_number_field.$wrapper.remove();
+		// The 'tracking_number' field is set because is the title field of the Doctype. So we remove from the 'standard_filters'
+		tracking_number_field.wrapper.remove();
 
-		// Update placeholder and help-text for Name field
-		name_field.$wrapper.attr('data-original-title', tracking_number_field.df.label);
-		name_field.$input.attr('placeholder', tracking_number_field.df.label);
+		// Set 'name' field tooltip and placeholder to match 'tracking_number' field
+		name_field.wrapper.setAttribute('data-original-title', tracking_number_field.df.label);
+		name_field.input.setAttribute('placeholder', tracking_number_field.df.placeholder);
 
-		// Override: onchange() method set in make_standard_filters(). We call refresh_list_view() if value has changed.
+		// Override: onchange() method set in frappe/list/base_list.js -> make_standard_filters()
 		name_field.df.onchange = customer_name_field.df.onchange = function () {
-			this.value = this.input.value = this.get_input_value().trim().toUpperCase();  // Change internal and UI value
+			// Remove '%' added in frappe/list/base_list.js -> get_standard_filters() when the listview loads and update both UI and internal values
+			this.value = this.input.value = this.get_input_value().replaceAll('%', '').trim().toUpperCase(); // this.set_input() is not working
+
 			if (this.value !== this.last_value) {
-				listview.filter_area.refresh_list_view(); // Same as make_standard_filters()
+				listview.filter_area.refresh_list_view(); // refresh_list_view() if the value has changed
 			}
-		}
+		};
 
-		// TODO: listview.get_count_str() => This call frappe.db.count() using 'filters' not 'or_filters'
-		// TODO: listview.list_sidebar.get_stats() => This call frappe.desk.reportview.get_sidebar_stats using 'filters' not 'or_filters'
-
-		// Override to add: or_filters and Remove '%' char added in get_standard_filters(Core function)
+		/* Override to add 'or_filters'. FIXME: Will be better to have it on backend to solve: gets_args + get_count_str + get_stats
+		 * frappe hook: 'permission_query_conditions' Wont work, It get called after we have build the frappe
+		 * check this: override_whitelisted_methods. The problem is this method is HEAVILY used
+		 * TODO: listview.get_count_str() => This call frappe.db.count() using 'filters' not 'or_filters'
+		 * TODO: listview.list_sidebar.get_stats() => This call frappe.desk.reportview.get_sidebar_stats using 'filters' not 'or_filters' */
 		listview.get_args = function () {
-			// Removing '%' added when the listview loads first time. Also sanitize field and change UI
-			name_field.value = name_field.input.value = name_field.get_input_value().replaceAll('%', '');
-            customer_name_field.value = customer_name_field.input.value = customer_name_field.get_input_value().replaceAll('%', '');
-
 			let args = frappe.views.ListView.prototype.get_args.call(listview);  // Calling his super for the args
 
 			const name_filter = args.filters.findIndex(f => f[1] === 'name');  // f -> ['Doctype', 'field', 'sql_search_term', 'value']
@@ -44,13 +45,13 @@ frappe.listview_settings['Parcel'] = {
 			}
 
 			return args;
-		}
+		};
 
 		// Override to add a dropdown. If the 'show' function within the 'button' property is null or returns False it won't create the 'settings_button'
 		listview.get_meta_html = function (doc) {
 			let $html = $(frappe.views.ListView.prototype.get_meta_html.call(listview, doc)); // Calling his super for the html string value
 
-			const urls = cargo_management.load_carrier_settings(doc.carrier).urls.reduce((acc, url) => {
+			const urls_html = cargo_management.load_carrier_settings(doc.carrier).urls.reduce((acc, url) => {
 				return acc + `<a class="dropdown-item" href="${url.url + doc.tracking_number}" target="_blank">${url.title}</a>`;
 			}, ''); // Loading the Carrier URLs then return a single string of items for the dropdown
 
@@ -58,12 +59,12 @@ frappe.listview_settings['Parcel'] = {
 			$html.find('.hidden-md.hidden-xs').html(`
 				<div class="list-actions dropdown">
 					<a class="btn btn-default btn-xs dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${__('Carriers')}</a>
-					<div class="dropdown-menu dropdown-menu-right">${urls}</div>
+					<div class="dropdown-menu dropdown-menu-right">${urls_html}</div>
 				</div>
 			`);
 
 			return $html.prop('outerHTML');
-		}
+		};
     },
 
 	// Unused: Light Blue. // TODO: Migrate to Document States? Maybe when frappe core starts using it.
@@ -86,6 +87,6 @@ frappe.listview_settings['Parcel'] = {
 
 	formatters: {
 		transportation: (value) => cargo_management.transportation_formatter(value),
-		name: (value, df, doc) => (value !== doc.tracking_number) ? value.bold() : ''
+		name: (value, df, doc) => (value !== doc.tracking_number) ? `<b>${value}</b>` : ''
 	}
 };
