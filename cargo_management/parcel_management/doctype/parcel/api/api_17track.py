ca13dd2ea5b3da5a1f0be3cf212ae9faba19b7e1
Jeans K. Real <jkrb7@hotmail.com>
2023-07-07 12:59:18 -0600

    17Track API Functional

--- cargo_management/parcel_management/doctype/parcel/api/api_17track.py
+++ cargo_management/parcel_management/doctype/parcel/api/api_17track.py
@@ -1,71 +1,164 @@
+import re
+from datetime import datetime as dt
 from types import SimpleNamespace
 
-import requests
+from requests import post
 
 import frappe
-
-
-class _17TrackAPIError(Exception):
-	""" Child Class to personalize API Error """
-	# https://api.17track.net/en/doc?anchor=list-of-error-codes&version=v2
-	pass
+from frappe.app import handle_exception
 
 
 class API17Track:
-	""" 17Track methods to control class API and parse data. """
-	api_base = "https://api.17track.net/track/v2/"
+	API_BASE: str = "https://api.17track.net/track/v2/"
 
 	# The Dict Keys are the actual String representation for Users, and the Dict Values are the carrier code for the API
 	# See More at: https://res.17track.net/asset/carrier/info/apicarrier.all.json
-	carrier_codes = {
+	CARRIER_CODES: dict = {
 		'Amazon': 100143,  # Swiship it works
 		'FedEx': 100003,
-		'LaserShip': 100052,
+		'OnTrac': 100049,
 		'Cainiao': 190271,
+		'SF Express': 190766,  # or: 100012 for intl
 		'Yanwen': 190012,
-		'YunExpress': 190008
+		'YunExpress': 190008,
+		'SunYou': 190072
 	}
 
-	def __init__(self):
+	# See List of status at: https://api.17track.net/en/doc?anchor=main-status-of-the-shipping-process&version=v2
+	STATUSES: dict = {
+		'Not Found': 'Unknown',
+		'Info Received': 'Pre Transit',
+		'Expired': 'Failure', 'Delivery Failure': 'Failure',
+		'Exception': 'Error'
+	}
+
+	SUB_STATUS_TO_STATUS: dict = {  # This Map 17Track SubStatus to Statuses
+		'Exception Returning': 'Return To Sender', 'Exception Returned': 'Return To Sender',
+		'Exception Cancel': 'Cancelled'
+	}
+
+	# 17Track Error Codes: https://api.17track.net/en/doc?anchor=list-of-error-codes&version=v2
+	TRACKING_REGISTERED = -18019901
+	TRACKING_NEED_REGISTER = -18019902
+	QUOTA_LIMIT = -18019908
+
+	def __init__(self, carrier: str) -> None:
 		self.data = {}
+		self.carrier = self.CARRIER_CODES.get(carrier)  # Fail Silently?
 		self.api_key = frappe.conf['17track_api_key']
 
-	def _build_request(self, endpoint, payload):
-		self.data = requests.request('POST', url=self.api_base + endpoint, json=payload, headers={
+	@staticmethod
+	def unscrub(txt: str) -> str:
+		""" Converts: OutForDelivery to Out For Delivery """
+		return ' '.join(re.findall('[A-Z][a-z]*', txt))
+
+	def _build_request(self, endpoint: str, payload: list):
+		response = post(url=f"{self.API_BASE + endpoint}", json=payload, headers={
 			"content-type": "application/json",
 			"17token": self.api_key
-		}).json(object_hook=lambda d: SimpleNamespace(**d)).data
+		}).json(object_hook=lambda d: SimpleNamespace(**d)).data  # Convert to Object and return the 'data' key
 
-		if self.data.rejected:
-			if self.data.rejected[0].error.code == -18019901:
-				raise _17TrackAPIError(self.data.rejected[0]['error']['message'])
+		if response.accepted:
+			return response.accepted[0]
 
-	def register_package(self, tracking_number, carrier):
-		""" Create a Tracking on 17Track """
-		response = self._build_request('register', payload=[{
+		match response.rejected[0].error.code:
+			case self.TRACKING_REGISTERED | self.TRACKING_NEED_REGISTER:
+				raise Exception(response.rejected[0].error.message)  # Default Throw
+			case self.QUOTA_LIMIT | _:
+				raise Exception(response.rejected[0].error)
+
+	def register_package(self, tracking_number: str):
+		""" Register a Tracking on 17Track """
+		unique_tag = frappe.generate_hash(tracking_number, length=15)
+
+		return self._build_request('register', payload=[{
 			"number": tracking_number,
-			"carrier": carrier,
-			"auto_detection": True,  # So we avoid sending the carrier?
+			"carrier": self.carrier,
+			"tag": unique_tag,  # CUSTOM: With this we know if the Parcel has been created on 17Track API
+			"auto_detection": False,  # So we avoid sending the carrier?
 		}])
 
-		response = response.json()  # IMPROVE THIS
-
-		if response['data']['accepted']:
-			return response['data']['accepted'][0]
-		elif response['data']['rejected']:
-			raise Exception(response['data']['rejected'][0]['error'])
-
-	def retrieve_package_data(self, tracking_number):
-		""" Retrieve data from 17Track """
-		response = self._build_request('gettrackinfo', payload=[{
+	def retrieve_package_data(self, tracking_number: str) -> dict:
+		""" Retrieve data from 17Track Tracking """
+		obj_17track = self._build_request('gettrackinfo', payload=[{
 			"number": tracking_number
 		}])
 
-		response = response.json()  # IMPROVE THIS
+		return self._build_parcel_data(obj_17track.track_info)
 
-		if response['data']['accepted']:
-			return response['data']['accepted'][0]
-		elif response['data']['rejected']:
-			raise Exception(response['data']['rejected'][0]['error'])
+	def convert_from_webhook(self, response):
+		""" Convert a dict from 17Track to our Parcel Object. """
+		obj_17track = SimpleNamespace(**response)
+		obj_17track.latest_status = SimpleNamespace(**response['latest_status'])
+		obj_17track.latest_event = SimpleNamespace(**response['latest_event'])
+		obj_17track.misc_info = SimpleNamespace(**response['misc_info'])
+		obj_17track.time_metrics = SimpleNamespace(**response['time_metrics'])
+		obj_17track.time_metrics.estimated_delivery_date = SimpleNamespace(**response['time_metrics']['estimated_delivery_date'])
 
-		return response
+		return self._build_parcel_data(obj_17track)
+
+	def _build_parcel_data(self, obj_17track) -> dict:
+		""" Build our Object(Parcel Document) from 17Track Data. """
+		self.data: dict = {
+			'carrier_status': self.STATUSES.get(stat := self.unscrub(obj_17track.latest_status.status), stat),  # Unscrub
+			'carrier_status_detail': self.unscrub(obj_17track.latest_status.sub_status),               # Normalize SubStatus
+			'carrier_est_weight': round(float(obj_17track.misc_info.weight_kg or 0.00) * 2.20462, 1),  # From KG to Pounds
+		}
+
+		# Change Status if the status_detail gives us more information about it!
+		if self.data['carrier_status'] == 'Error':  # Originally 'Exception'. See STATUS_MAP
+			self.data['carrier_status'] = self.SUB_STATUS_TO_STATUS.get(self.data['carrier_status_detail'], self.data['carrier_status'])
+
+		# Searching for the carrier_est_delivery. If it comes in local time or comes generalized
+		if (date := getattr(obj_17track.time_metrics.estimated_delivery_date, 'from')) \
+			or (date := getattr(obj_17track.time_metrics.estimated_delivery_date, 'to')):
+			self.data['carrier_est_delivery'] = dt.fromisoformat(date).replace(tzinfo=None)
+
+			if self.data['carrier_est_delivery'].time() == dt.min.time():  # If midnight, set to end of the day!
+				self.data['carrier_est_delivery'] = self.data['carrier_est_delivery'].replace(hour=23, minute=59)
+
+		# Build the latest event detail. If 'Delivered', get the real delivery date
+		if last_event := obj_17track.latest_event:
+			self.data['carrier_last_detail'] = (
+				f"<b>{self.unscrub(last_event.stage or obj_17track.latest_status.status)}</b><br><br>"
+				f"{last_event.description or 'Without Description'}<br><br>"
+				f"{last_event.location or ((last_event.address.city or '') + (last_event.address.state or '') + (last_event.address.postal_code or ''))}"
+			)
+
+			# If parcel is Delivered we get the 'real_delivery_date' from the Latest Event Timestamp ISO
+			if self.data['carrier_status'] == 'Delivered':
+				self.data['carrier_real_delivery'] = dt.fromisoformat(last_event.time_iso).replace(tzinfo=None)
+
+		return self.data
+
+
+@frappe.whitelist(allow_guest=True, methods='POST')
+def webhook_17track(**kwargs):
+	""" POST To: {URL}/api/method/cargo_management.parcel_management.doctype.parcel.api.api_17track.webhook_17track """
+	kwargs.pop('cmd')  # Remove extra data added by Frappe
+	frappe.session.user = '17Track API'  # Quick Hack. Very useful
+
+	try:
+		# TODO: SIGN
+
+		if kwargs['event'] != 'TRACKING_UPDATED':
+			return 'Not a Tracker Update Webhook Event'  # TODO: Make something with this?
+
+		parcel = frappe.get_doc('Parcel', kwargs['data']['number'])  # Search Parcel using 'name' only
+	except (KeyError, frappe.DoesNotExistError) as e:
+		frappe.log_error(
+			f"17Track Webhook: {type(e).__name__} -> {e}",
+			reference_doctype='Parcel', reference_name=kwargs.get('data', {}).get('number', None)
+		)
+
+		return handle_exception(e)  # Handle as same as in frappe/app.py | handles -> http_status_code
+	else:
+		data = API17Track(carrier=parcel.carrier).convert_from_webhook(kwargs['data']['track_info'])  # This creates obj
+
+		parcel.update_from_api_data(api_data=data)
+
+		# Set because doc will be saved from webhook data. No validations needed.
+		parcel.flags.ignore_validate = parcel.flags.ignore_mandatory = parcel.flags.ignore_links = True
+		parcel.save(ignore_permissions=True)
+
+		return f"Parcel {parcel.tracking_number} updated."
