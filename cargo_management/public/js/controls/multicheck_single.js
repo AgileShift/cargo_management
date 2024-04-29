4b88325d8d35cd2408f795b9ac9d545eed78807b
Jeans K. Real <jkrb7@hotmail.com>
2024-04-29 15:52:14 -0600

    Minor changes to handle better values

--- cargo_management/public/js/controls/multicheck_single.js
+++ cargo_management/public/js/controls/multicheck_single.js
@@ -1,5 +1,5 @@
 frappe.ui.form.ControlMultiCheckSingle = class ControlMultiCheckSingle extends frappe.ui.form.ControlMultiCheck {
-	// Override ControlMultiCheck to create a MultiCheckSingle -> See: frappe/form/controls/text.js
+	// Extends ControlMultiCheck to create a MultiCheckSingle see: /form/controls/multicheck.js
 
 	refresh_input() {
 		// Override this method, We drew inspiration from: /form/controls/base_input.js -> refresh_input();
@@ -18,23 +18,20 @@ frappe.ui.form.ControlMultiCheckSingle = class ControlMultiCheckSingle extends f
 				this.$checkbox_area.find('input').not($checkbox).prop('checked', false); // Uncheck Others
 				this.selected_options = $checkbox.attr("data-unit");
 			} else {
-				this.selected_options = ''; // If unchecked, set to empty
+				this.selected_options = null; // If unchecked, set to empty
 			}
 
 			if (this.df.reqd) {
 				this.set_invalid(); // If required, mark or unmark field as invalid
 			}
 
+			// To pass through the selected value
+			this.last_value = this.value;
+			this.value = this.selected_options;
+
 			// Call the on_change function with the selected option. This is useful for binding to other fields.
 			this.df.on_change && this.df.on_change(this.selected_options);
 		});
-	}
-
-	set_input(value) {
-		console.log('set_input')
-		// Extend to set the selected option
-		// Refer to: /form/controls/multicheck.js -> set_checked_options();
-		this.set_checked_options();
 	}
 
 	// Refer to: /form/controls/base_input.js -> set_mandatory() & set_invalid() & set_required() & set_bold()
@@ -46,7 +43,7 @@ frappe.ui.form.ControlMultiCheckSingle = class ControlMultiCheckSingle extends f
 		this.$wrapper.toggleClass("has-error", !this.selected_options);
 	}
 	set_required() {
-		this.$label.toggleClass('reqd', this.df.reqd);
+		this.$label.toggleClass('reqd', Boolean(this.df.reqd));
 	}
 	set_bold() {
 		this.$checkbox_area.toggleClass('bold', this.df.bold);
