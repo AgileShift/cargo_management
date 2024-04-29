74f1cfbcb4faf4150bba66d7104fe21ddf1d38d4
Jeans K. Real <jkrb7@hotmail.com>
2024-04-29 15:56:43 -0600

    ParcelQuickEntry is now configured to use the New Transportation Controler. Now we can use default values and check for reqd(Mandatory)

--- cargo_management/public/js/utils/parcel_quick_entry.js
+++ cargo_management/public/js/utils/parcel_quick_entry.js
@@ -6,9 +6,14 @@ frappe.ui.form.ParcelQuickEntryForm = class ParcelQuickEntryForm extends frappe.
 	}
 
 	render_dialog() {
+		// FIXME: Look For a Place to Override Parcel Form Control
+		frappe.meta.docfield_map['Parcel']['transportation'].fieldtype = 'TransportationMultiCheck';
+
 		this.add_extra_fields(); // Adding Table Field after executing super.is_quick_entry();
 		super.render_dialog();
 		this.init_post_render_dialog_operations(); // Custom method to modify DOM properties after the dialog has been rendered
+
+		frappe.meta.docfield_map['Parcel']['transportation'].fieldtype = 'Select'; // FIXME: Resetting the fieldtype to 'Select' to avoid problems in other parts of the system(ListView Filter and Form Field)
 	}
 
 	add_extra_fields() {
@@ -20,22 +25,22 @@ frappe.ui.form.ParcelQuickEntryForm = class ParcelQuickEntryForm extends frappe.
 		this.mandatory.splice(10, 0, {fieldtype: 'Column Break'}); // Split 'Shipper' and [Number of Order, Purchase Date]
 
 		this.mandatory.splice(13, 0, {fieldtype: 'Section Break', fieldname: 'content_section', hide_border: true}, {
-		 	fieldtype: 'Table', fieldname: 'content', options: 'Parcel Content', in_place_edit: false, fields: [
+			fieldtype: 'Table', fieldname: 'content', options: 'Parcel Content', in_place_edit: false, fields: [
 		 		{label: __('Description'), fieldtype: 'Small Text', fieldname: 'description', in_list_view: true, max_height: '4rem', columns: 6},
 		 		{label: __('Tracking Number'), fieldtype: 'Data', fieldname: 'tracking_number', in_list_view: true, columns: 2},
 		 		{label: __('Item Code'), fieldtype: 'Link', fieldname: 'item_code', options: 'Item', in_list_view: true, columns: 2}
-		 	]}); // TODO: Item code with filters. Maybe we can filter if its is by the service Type?
-
+		 	]}
+		); // TODO: Item code with filters. Maybe we can filter if its is by the service Type?
 	}
 
 	init_post_render_dialog_operations() {
-		let {carrier, tracking_number, extra_services_section, content_section, content, transportation} = this.dialog.fields_dict;
+		let {tracking_number, transportation, carrier, extra_services_section, content_section, content} = this.dialog.fields_dict;
 
 		tracking_number.df.onchange = function () {  // Override onchange to sanitize field and set carrier
 			const data = cargo_management.find_carrier_by_tracking_number(this.get_input_value());
 
-			carrier.set_input(data.carrier);       // Update the Carrier field
-			this.set_input(data.tracking_number);  // Tracking Number returned is sanitized
+			carrier.set_input(data.carrier);      // Update the Carrier field
+			this.set_input(data.tracking_number); // Tracking Number returned is sanitized
 		};
 
 		this.dialog.$wrapper.find('.modal-dialog').addClass('modal-lg'); // Making the Dialog large
