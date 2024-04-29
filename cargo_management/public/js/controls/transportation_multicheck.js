67b0740ffd7b56455aa6c57fe591c77ffeef5dc3
Jeans K. Real <jkrb7@hotmail.com>
2024-04-29 15:53:14 -0600

    Transportation MultiCheck Control Inherits from MultiCheck with a touch for Single Option Selection

--- cargo_management/public/js/controls/transportation_multicheck.js
+++ cargo_management/public/js/controls/transportation_multicheck.js
@@ -1,11 +1,32 @@
 frappe.ui.form.ControlTransportationMultiCheck = class ControlTransportationMultiCheck extends frappe.ui.form.ControlMultiCheckSingle {
 	// Extends from ControlMultiCheckSingle to create a 'TransportationMultiCheck'
+	// TODO: Create a Control from scratch to use Radio Buttons instead of Checkboxes
 
 	parse_df_options() {
-		// No need to call -> super(). See: /form/controls/multicheck.js -> parse_df_options();
+		// No need to call -> super(). See: /form/controls/multicheck.js -> parse_df_options(); From the parent class
+
+		// 'checked': Gives Error if a value not the default is selected in quickEntry. It shows selected the default always
 		this.options = this.df.options.split('\n').map(t => ({
-			value: t, description: __(t),
-			label: __(t).toUpperCase() + cargo_management.icon_html(cargo_management.TRANSPORTATIONS[t].icon)
+			value: t, description: __(t), checked: t === this.df.default,
+			label: __(t).toUpperCase() + cargo_management.icon_html(cargo_management.TRANSPORTATIONS[t].icon),
+			color: cargo_management.TRANSPORTATIONS[t].color
 		}));
+	}
+
+	get_checkbox_element(option) {
+		let ele = super.get_checkbox_element(option);
+
+		ele.css('border', `1px solid ${option.color}`);
+		ele.find('label').css('color', option.color);
+
+		return ele;
+	}
+
+	// Refer to: /form/controls/check.js -> set_input();
+	set_input(value) {
+		this.last_value = this.value;
+		this.value = value;
+
+		this.set_checked_options();  // Set Initial Value. Refer to: /form/controls/multicheck.js -> set_checked_options();
 	}
 }
