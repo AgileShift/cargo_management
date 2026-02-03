0db4c9efe43719f4619a4581f9eaa77a37a8e0e6
Jeans K. Real <jkrb7@hotmail.com>
2026-02-02 20:31:11 -0600

    Remove Button, and generate Python Class Descriptor

--- cargo_management/shipment_management/doctype/cargo_shipment_warehouse/cargo_shipment_warehouse.py
+++ cargo_management/shipment_management/doctype/cargo_shipment_warehouse/cargo_shipment_warehouse.py
@@ -2,4 +2,22 @@ from frappe.model.document import Document
 
 
 class CargoShipmentWarehouse(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		height: DF.Float
+		length: DF.Float
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		reference: DF.Data
+		weight: DF.Float
+		width: DF.Float
+	# end: auto-generated types
+
 	pass
