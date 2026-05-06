a5866965c4a80b9afbdab9bc54229eff8790af2c
Jeans K. Real <jkrb7@hotmail.com>
2026-05-06 11:56:47 -0600

    Feat: Warehouse Receipt Improved Calculations and correct Fields

--- cargo_management/warehouse_management/doctype/warehouse_receipt_line/warehouse_receipt_line.py
+++ cargo_management/warehouse_management/doctype/warehouse_receipt_line/warehouse_receipt_line.py
@@ -10,19 +10,25 @@ class WarehouseReceiptLine(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		carrier_delivered_at: DF.Datetime | None
+		carrier_weight: DF.Float
+		customer: DF.Link | None
+		customer_name: DF.ReadOnly | None
 		height: DF.Float
 		length: DF.Float
 		parcel: DF.Link
-		parcel_customer: DF.Link | None
-		parcel_customer_name: DF.ReadOnly | None
-		parcel_transportation: DF.ReadOnly | None
+		parcel_transportation: DF.Literal["Sea", "Air"]
 		parent: DF.Data
 		parentfield: DF.Data
 		parenttype: DF.Data
 		type: DF.Literal["", "Box", "Envelope", "Pallet", "Mail"]
-		volumetric_weight: DF.Float
-		warehouse_est_weight: DF.Float
+		warehouse_weight: DF.Float
 		width: DF.Float
 	# end: auto-generated types
 
 	pass
+
+	@property
+	def volumetric_weight(self):
+		return self.length * self.width * self.height
+
