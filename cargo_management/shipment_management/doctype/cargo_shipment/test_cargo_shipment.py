21bbd3d1ec2fcc258006aee0b29f53efd4483e87
Jeans K. Real <jkrb7@hotmail.com>
2026-05-15 23:27:02 -0600

    Test Compliance v16

--- cargo_management/shipment_management/doctype/cargo_shipment/test_cargo_shipment.py
+++ cargo_management/shipment_management/doctype/cargo_shipment/test_cargo_shipment.py
@@ -1,5 +1,17 @@
-import unittest
+from frappe.tests import IntegrationTestCase
 
 
-class TestShipment(unittest.TestCase):
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record dependencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+class IntegrationTestCargoShipment(IntegrationTestCase):
+	"""
+	Integration tests for CargoShipment.
+	Use this class for testing interactions between multiple components.
+	"""
+
 	pass
