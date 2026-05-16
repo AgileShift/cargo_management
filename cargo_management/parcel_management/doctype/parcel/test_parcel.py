e5f7347f0630a2161b5d68c356061f86332480ca
Jeans K. Real <jkrb7@hotmail.com>
2026-05-15 23:21:21 -0600

    Test Compliance v16

---
+++ cargo_management/parcel_management/doctype/parcel/test_parcel.py
@@ -0,0 +1,17 @@
+from frappe.tests import IntegrationTestCase
+
+
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record dependencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+class IntegrationTestParcel(IntegrationTestCase):
+	"""
+	Integration tests for Parcel.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
