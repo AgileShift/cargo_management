1544aef810ecc9b4faad0531fed6a1d153be0858
Jeans K. Real <jkrb7@hotmail.com>
2026-06-04 16:19:33 -0600

    Remove Unused fields, this now live on Warehouse Receipt. Please do your migrations

--- cargo_management/shipment_management/doctype/cargo_shipment_line/cargo_shipment_line.py
+++ cargo_management/shipment_management/doctype/cargo_shipment_line/cargo_shipment_line.py
@@ -10,25 +10,19 @@ class CargoShipmentLine(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		carrier: DF.Link | None
 		carrier_est_weight: DF.Float
 		carrier_real_delivery: DF.Datetime | None
 		customer: DF.Link | None
 		customer_name: DF.Data | None
 		description: DF.SmallText | None
-		height: DF.Float
-		length: DF.Float
 		parcel: DF.Link
 		parent: DF.Data
 		parentfield: DF.Data
 		parenttype: DF.Data
 		received_date: DF.Date | None
 		transportation: DF.Literal["Sea", "Air"]
-		type: DF.Literal["Box", "Envelope", "Pallet"]
-		volumetric_weight: DF.Float
 		warehouse_est_weight: DF.Float
 		warehouse_receipt: DF.Link | None
-		width: DF.Float
 	# end: auto-generated types
 
 	pass
