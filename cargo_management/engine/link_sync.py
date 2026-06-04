c4d5315ffc5733b74e943dfe37f294ad2738845d
Jeans K. Real <jkrb7@hotmail.com>
2026-06-04 16:20:15 -0600

    Perf: frappe.db.get frappe.db.set n+1 Query, is fixed

--- cargo_management/engine/link_sync.py
+++ cargo_management/engine/link_sync.py
@@ -36,17 +36,13 @@ class LinkSyncMixin:
 			current_names = set(pluck_child_field(self.get(rule.child_table), rule.child_link_field))
 			previous_names_by_rule = getattr(self, "_previous_link_sync_names", {})
 			previous_names = previous_names_by_rule.get(rule, self._get_previous_link_names(rule))
+			current_links = self._get_current_links(rule, current_names)
 
-			for target_name in current_names:
-				if frappe.db.get_value(rule.target_doctype, target_name, rule.target_link_field) != self.name:
-					frappe.db.set_value(
-						rule.target_doctype,
-						target_name,
-						rule.target_link_field,
-						self.name,
-						update_modified=False,
-					)
-
+			self._set_links(rule, [
+				target_name
+				for target_name in current_names
+				if current_links.get(target_name) != self.name
+			])
 			self._clear_removed_links(rule, previous_names - current_names)
 
 	def unlink_synced_links(self):
@@ -63,15 +59,47 @@ class LinkSyncMixin:
 		return set(pluck_child_field(doc_before_save.get(rule.child_table), rule.child_link_field))
 
 	def _clear_removed_links(self, rule, target_names):
-		for target_name in target_names:
-			if frappe.db.get_value(rule.target_doctype, target_name, rule.target_link_field) == self.name:
-				frappe.db.set_value(
-					rule.target_doctype,
-					target_name,
-					rule.target_link_field,
-					None,
-					update_modified=False,
-				)
+		target_names = list(set(target_names))
+		if not target_names:
+			return
+
+		target = frappe.qb.DocType(rule.target_doctype)
+		target_link_field = getattr(target, rule.target_link_field)
+
+		(
+			frappe.qb.update(target)
+			.set(target_link_field, None)
+			.where(target.name.isin(target_names))
+			.where(target_link_field == self.name)
+		).run()
+
+	def _set_links(self, rule, target_names):
+		target_names = list(set(target_names))
+		if not target_names:
+			return
+
+		target = frappe.qb.DocType(rule.target_doctype)
+		target_link_field = getattr(target, rule.target_link_field)
+
+		(
+			frappe.qb.update(target)
+			.set(target_link_field, self.name)
+			.where(target.name.isin(target_names))
+		).run()
+
+	def _get_current_links(self, rule, target_names):
+		target_names = list(set(target_names))
+		if not target_names:
+			return {}
+
+		return {
+			row.name: row.get(rule.target_link_field)
+			for row in frappe.get_all(
+				rule.target_doctype,
+				filters={"name": ["in", target_names]},
+				fields=["name", rule.target_link_field],
+			)
+		}
 
 	def _validate_duplicate_links(self, rule, target_names):
 		seen, duplicates = set(), set()
@@ -94,8 +122,7 @@ class LinkSyncMixin:
 	def _validate_existing_links(self, rule, target_names):
 		conflicts = []
 
-		for target_name in set(target_names):
-			current_link = frappe.db.get_value(rule.target_doctype, target_name, rule.target_link_field)
+		for target_name, current_link in self._get_current_links(rule, target_names).items():
 			if current_link and current_link != self.name:
 				conflicts.append((target_name, current_link))
 
